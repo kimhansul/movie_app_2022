6f5d537ddc7811d3ba2f6df4e0f3060f73a4d9c2
kimhansul <gkstmf7506@gmail.com>
2022-04-15 11:49:33 +0900

    05 깃허브에 리액트 앱 업로드하기_로딩화면추가

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import React from 'react';
 
 class App extends React.Component{
-  constructor(props){
-    super(props)
-    console.log('constructor 실행완료');
+  state={
+    isLoding: true,
+    movies:[]
   }
 
-  state={count: 0};
-  add=()=>{
-    this.setState({count: this.state.count+1});
-  }
-  minus=()=>{
-    this.setState({count: this.state.count-1});
-  }
   componentDidMount(){
-    console.log('componentDidMount 실행완료');
+    setTimeout(()=>{
+      this.setState({
+        isLoding: false
+      })
+    },5000)
   }
-  componentDidUpdate(){
-    console.log('componentDidUpdate 실행완료');
-  }
-  componentWillUnmount(){
-    console.log('뭔데');
-  }
+
   render(){
-    console.log('render 실행완료');
-    return (
-    <div>
-      <h1>번호는 {this.state.count}</h1>
-      <button onClick={this.add}>더하기</button>
-      <button onClick={this.minus}>빼기</button>
-    </div>
+    const {isLoding}=this.state;
+    return(
+      <div>
+        {isLoding ? '로딩중...' : '로딩완료'}
+      </div>
     );
   }
 }
