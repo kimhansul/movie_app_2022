6dae8200e5e41d758bbd1d35afa54bb1b623b27c
kimhansul <gkstmf7506@gmail.com>
2022-04-19 11:37:26 +0900

    06 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component{
   state={
@@ -8,7 +10,7 @@ class App extends React.Component{
   }
 
   getMovies= async ()=>{
-    const {data: {data: {movies},},}= await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    const {data: {data: {movies},},}= await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
     this.setState({movies, isLoding: false});
   }
 
@@ -17,11 +19,24 @@ class App extends React.Component{
   }
 
   render(){
-    const {isLoding}=this.state;
+    const {isLoding , movies}=this.state;
     return(
-      <div>
-        {isLoding ? '로딩중...' : '로딩완료'}
-      </div>
+      <section className='container'>
+        {isLoding ? (<div className='loader'><span className='loader__text'>'로딩중...'</span></div>) : 
+          (<div className='movies'>
+            {movies.map((movie)=>{
+              return <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />;
+            })}
+          </div>)
+        }
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,21 @@
 import react from 'react';
 import propTypes from 'prop-types';
+import "./Movie.css";
 
-function Movie(){
+function Movie({title,year,summary,poster}){
     return (
-        <h1></h1>
+        <div className='movie__data'>
+            <img src={poster} alt={title} title={title}></img>
+            <h3 className='movie__title'>{title}</h3>
+            <h5 className='movie__year'>{year}</h5>
+            <p className='movie__summary'>{summary}</p>
+        </div>
     );
 }
 
 Movie.propTypes={
-    id: propTypes.number.isRequired,
     year: propTypes.number.isRequired,
     title: propTypes.string.isRequired,
-    summary: propTypes.string.isRequired,
     summary: propTypes.string.isRequired,
     poster: propTypes.string.isRequired,
 };
