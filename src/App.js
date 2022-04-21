938421c9e70610dd2930fa7a5fcb42b2849017a2
kimhansul <gkstmf7506@gmail.com>
2022-04-21 12:32:22 +0900

    리액트영화앱 라우터, 네비게이션 구현

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from "react";
 import axios from "axios";
-import Movie from "./Movie";
+import Home from "./routes/Home"
+import {HashRouter, Route} from "react-router-dom";
+import About from "./routes/About"
+import Navigation from "./components/Navigation"
 import "./App.css";
 
-class App extends React.Component{
-  state={
-    isLoding: true,
-    movies:[]
-  }
-
-  getMovies= async ()=>{
-    const {data: {data: {movies},},}= await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    this.setState({movies, isLoding: false});
-  }
-
-  componentDidMount(){
-    this.getMovies();
-  }
-
-  render(){
-    const {isLoding , movies}=this.state;
-    return(
-      <section className='container'>
-        {isLoding ? (<div className='loader'><span className='loader__text'>'로딩중...'</span></div>) : 
-          (<div className='movies'>
-            {movies.map((movie)=>{
-              return <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />;
-            })}
-          </div>)
-        }
-      </section>
-    );
-  }
+function App(){
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  )
 }
 
 export default App;
